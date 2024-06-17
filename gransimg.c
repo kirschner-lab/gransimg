654e659a93be4262023001ec447b90fa057fa610
Pariksheet Nanda <pnanda@umich.edu>
2024-06-17 19:37:26 -0400

    ENH: Parse CLI arguments

--- gransimg.c
+++ gransimg.c
@@ -18,19 +18,52 @@
  */
 
 #include <string.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <errno.h>
 
 #include <tiffio.h>
 #include <omp.h>
 
 
-/** Change this to your GranSim grid size */
+/** Change this to your GranSim grid size. */
 const int SZ = 300;
+
+/** Change this to your GranSim timesteps-per-day. */
+const int TS_PER_DAY = 144;
 
 typedef enum {
   AGENT,
   CHEMOKINE
 } im_t;
 
+/** Store program options. */
+struct opts_s {
+  char* i;
+  char* o;
+  char* e;
+  char* d;
+} opts_default = {".", "img", "0", "0"};
+typedef struct opts_s opts_t;
+
+/** Program usage. */
+const char USAGE[] = "Usage: gransimg [--help] [--test] [--input DIR_INPUT] "
+  "[--output DIR_OUTPUT]\n"
+  "                [--exps LIST_EXPS] [--days LIST_DAYS]\n\n"
+  "Convert GranSim agent and grid dumps to TIFF images.  Images from each\n"
+  "experiment are created in parallel using OpenMP.\n\n"
+  "options:\n"
+  "--help    Show this message and exit.\n"
+  "--test    Only generate two test images: agent.tif, chemokine.tif without\n"
+  "          using any parallelism.\n"
+  "--input   Path to the model run directory (default: .)\n"
+  "--output  Path to the TIFF image output directory (default: img).\n"
+  "--exps    String of integers separated by non-numeric characters without\n"
+  "          corresponding replicate integers (default: 0 for all).  All\n"
+  "          replicates are run and individual replicates are not currently\n"
+             "supported.\n"
+  "--days    String of integers separated by non-numeric characters\n"
+  "          (default: 0 for the last day).\n";
 
 /**
  * @internal
@@ -164,6 +197,76 @@ write_im_chemokine(char* name, float* im) {
 
 
 /**
+ * @internal
+ * @brief Parse CLI arguments.
+ *
+ * @param argc Passed from main().
+ * @param argv Passed from main().
+ */
+void
+parse_args(opts_t* opts, int argc, char* argv[])
+{
+  /* Populate the option structs described in `man 3 getopt_long`. */
+  struct option longopts[] = {
+    {"help", no_argument, NULL, 'h'},
+    {"test-args", no_argument, NULL, 'a'},
+    {"test-imgs", no_argument, NULL, 't'},
+    {"input", required_argument, NULL, 'i'},
+    {"output", required_argument, NULL, 'o'},
+    {"exps", required_argument, NULL, 'e'},
+    {"days", required_argument, NULL, 'd'},
+    {NULL, 0, NULL, 0}
+  };
+
+  int test_args = 0;
+  int ch;
+  while ((ch = getopt_long(argc, argv, "", longopts, NULL)) != -1) {
+    switch(ch) {
+    case 'h':
+      printf(USAGE);
+      exit(0);
+    case 't':
+      {
+	uint32_t im_a[SZ * SZ];
+	float im_c[SZ * SZ];
+	for (uint32_t i = 0; i < SZ * SZ; ++i) {
+	  im_a[i] = i;
+	  im_c[i] = ((float) i) / (SZ * SZ);
+	}
+	write_im_agent("agent.tif", im_a);
+	write_im_chemokine("chemokine.tif", im_c);
+	exit(0);
+      }
+    case 'a':
+      test_args = 1;
+      break;
+    case 'i':
+      opts->i = optarg;
+      break;
+    case 'o':
+      opts->o = optarg;
+      break;
+    case 'e':
+      opts->e = optarg;
+      break;
+    case 'd':
+      opts->d = optarg;
+      break;
+    default:
+      printf(USAGE);
+      exit(1);
+    }
+  }
+
+  if (test_args) {
+    printf("input: %s\noutput: %s\nexps: %s\ndays: %s\n",
+	   opts->i, opts->o, opts->e, opts->d);
+    exit(0);
+  }
+}
+
+
+/**
  * @brief Export model directory agents and grid dumps to TIFF.
  *
  * @param name Path to the TIFF file to create or overwrite
@@ -174,17 +277,8 @@ write_im_chemokine(char* name, float* im) {
 int
 main(int argc, char* argv[])
 {
-  if (argc == 2 &&
-      strncmp(argv[1], "--test", 6)) {
-    uint32_t im_a[SZ * SZ];
-    float im_c[SZ * SZ];
-    for (uint32_t i = 0; i < SZ * SZ; ++i) {
-      im_a[i] = i;
-      im_c[i] = ((float) i) / (SZ * SZ);
-    }
-    write_im_agent("a.tif", im_a);
-    write_im_chemokine("c.tif", im_c);
-  }
-  
+  opts_t opts = opts_default;
+  parse_args(&opts, argc, argv);
+
   return 0;
 }
